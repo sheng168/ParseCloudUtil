81ef2bfb8fb69b658e9dfb5d89604a031f1d18ac
sheng168 <github@jsy.us>
2013-11-05 11:24:27 -0500

    Create unique.js

---
+++ cloud/unique.js
@@ -0,0 +1,23 @@
+var BusStop = Parse.Object.extend("BusStop");
+
+// Check if stopId is set, and enforce uniqueness based on the stopId column.
+Parse.Cloud.beforeSave("BusStop", function(request, response) {
+  if (!request.object.get("stopId")) {
+    response.error('A BusStop must have a stopId.');
+  } else {
+    var query = new Parse.Query(BusStop);
+    query.equalTo("stopId", request.object.get("stopId"));
+    query.first({
+      success: function(object) {
+        if (object) {
+          response.error("A BusStop with this stopId already exists.");
+        } else {
+          response.success();
+        }
+      },
+      error: function(error) {
+        response.error("Could not validate uniqueness for this BusStop object.");
+      }
+    });
+  }
+});
